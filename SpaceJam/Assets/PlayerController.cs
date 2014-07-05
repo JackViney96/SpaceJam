9e927afaeaa12dcd6760f0aebdffaf3fa8f706d3
dee-alpaca <arnold.m.dsilva@gmail.com>
2014-07-05 23:32:54 +1000

    --Arnold's 1st Commit --Added Player Movement (Issues?)

---
+++ SpaceJam/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Movement ();
+	}
+	void Movement() {
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);	
+			//transform.eulerAngles = new Vector2(0, 0); //Sprite Faces Right
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+			//transform.eulerAngles = new Vector2(0, 180); //Sprite Faces Left
+		}
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector2.up * 4f * Time.deltaTime);	
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (-Vector2.up * 4f * Time.deltaTime);
+		}
+	}
+}
