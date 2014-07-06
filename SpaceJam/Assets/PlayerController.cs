f210946dd084bf62d0d6bdb42c1b823328584fab
Jack - LAPTOP <jack.github.inbox@gmail.com>
2014-07-06 18:44:59 +1000

    Changes to movement: - Physical force is now applied rather than transformations

--- SpaceJam/Assets/PlayerController.cs
+++ SpaceJam/Assets/PlayerController.cs
@@ -2,25 +2,28 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	
+
+	public float speed = 100;
+
 	// Update is called once per frame
 	void Update () {
 		Movement ();
 	}
 	void Movement() {
-		if (Input.GetKey (KeyCode.D)) {
-			transform.Translate (Vector2.right * 4f * Time.deltaTime);	
+		if (Input.GetKey (KeyCode.W)) {
+			rigidbody.AddForce(Vector3.up * (speed * Time.deltaTime));	
 			//transform.eulerAngles = new Vector2(0, 0); //Sprite Faces Right
 		}
-		if (Input.GetKey (KeyCode.A)) {
-			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.S)) {
+			rigidbody.AddForce(-Vector3.up * (speed * Time.deltaTime));
 			//transform.eulerAngles = new Vector2(0, 180); //Sprite Faces Left
 		}
-		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (Vector2.up * 4f * Time.deltaTime);	
+		if (Input.GetKey (KeyCode.D)) {
+			rigidbody.AddForce(Vector3.right * (speed * Time.deltaTime));
+			//transform.Translate (Vector2.up * 4f * Time.deltaTime);	
 		}
-		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (-Vector2.up * 4f * Time.deltaTime);
+		if (Input.GetKey (KeyCode.A)) {
+			rigidbody.AddForce(-Vector3.right * (speed * Time.deltaTime));
 		}
 	}
 }
