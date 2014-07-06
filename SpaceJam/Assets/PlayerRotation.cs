ee4ae384840b4ada2b001919ef4c906d1bdaafb6
PritchardGSD <jack.github.inbox@gmail.com>
2014-07-06 23:14:29 +1000

    Attempts to fix rotation

--- SpaceJam/Assets/PlayerRotation.cs
+++ SpaceJam/Assets/PlayerRotation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿
+/*using UnityEngine;
 using System.Collections;
 
 public class PlayerRotation : MonoBehaviour {
@@ -7,7 +8,7 @@ public class PlayerRotation : MonoBehaviour {
 	void Update () {
 		/*Vector3 mousePos = Input.mousePosition;
 		transform.LookAt(mousePos);
-		*/
+
 		//rotation
 		Vector3 mousePos = Input.mousePosition;
 		mousePos.z = 5.23f;
@@ -20,5 +21,36 @@ public class PlayerRotation : MonoBehaviour {
 		//transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 		rigidbody.AddRelativeTorque(new Vector3(0, 0, angle));
 
+	}
+}
+*/
+using UnityEngine;
+using System.Collections;
+
+// @robotduck 2011
+// set the object's rigidbody angular drag to a high value, like 10
+
+public class PlayerRotation : MonoBehaviour {
+	
+	public Transform target;
+	public float force = 0.1f;
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Vector3 targetDelta = target.position - transform.position;
+		
+		//get the angle between transform.forward and target delta
+		float angleDiff = Vector3.Angle(transform.forward, targetDelta);
+		
+		// get its cross product, which is the axis of rotation to
+		// get from one vector to the other
+		Vector3 cross = Vector3.Cross(transform.forward, targetDelta);
+		
+		// apply torque along that axis according to the magnitude of the angle.
+		rigidbody.AddTorque(cross * angleDiff * force);
+		
+		
 	}
 }
---
+++ SpaceJam/Assets/followmouse.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followmouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos = Input.mousePosition;
+		mousePos.z = 5.23f;
+		transform.position = mousePos;
+	}
+}
